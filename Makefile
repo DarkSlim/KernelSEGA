4a65d567dc9842e4cfab765bcf3b8cd061173858
jsega <jsega@j.(none)>
2012-10-23 16:30:00 +0530

    Test 1 (ext4,NTFS,Disabled NFS,KGSL fix and more)

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 VERSION = 2
 PATCHLEVEL = 6
 SUBLEVEL = 32
-EXTRAVERSION = .9Kernel_SEGA
+EXTRAVERSION = .21-Kernel_SEGA
 NAME = Man-Eating Seals of Antiquity
 
 # *DOCUMENTATION*
@@ -344,7 +344,7 @@ KBUILD_CPPFLAGS := -D__KERNEL__
 KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -Wno-uninitialized \
 		   -fno-strict-aliasing -fno-common \
-		   -Werror-implicit-function-declaration \
+		   -Wimplicit-function-declaration \
 		   -Wno-format-security \
 		   -fno-delete-null-pointer-checks 
 KBUILD_AFLAGS   := -D__ASSEMBLY__
@@ -524,7 +524,7 @@ all: vmlinux
 ifdef CONFIG_CC_OPTIMIZE_FOR_SIZE
 KBUILD_CFLAGS	+= -Os
 else
-KBUILD_CFLAGS	+= -O2
+KBUILD_CFLAGS	+= -O4
 endif
 
 include $(srctree)/arch/$(SRCARCH)/Makefile
--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1587,7 +1587,7 @@ static inline void *__vmalloc_node_flags(unsigned long size,
 					int node, gfp_t flags)
 {
 	return __vmalloc_node(size, 1, flags, PAGE_KERNEL,
-					node, __builtin_return_address(0));
+				node, __builtin_return_address(0));
 }
 
 /**
@@ -1607,21 +1607,22 @@ void *vmalloc(unsigned long size)
 EXPORT_SYMBOL(vmalloc);
 
 /**
- *	vzalloc_zram - allocate virtually contiguous memory with zero fill
- *	@size:	allocation size
- *	Allocate enough pages to cover @size from the page level
- *	allocator and map them into contiguous kernel virtual space.
- *	The memory allocated is set to zero.
+ *      vzalloc - allocate virtually contiguous memory with zero fill
+ *      @size:  allocation size
+ *      Allocate enough pages to cover @size from the page level
+ *      allocator and map them into contiguous kernel virtual space.
+ *      The memory allocated is set to zero.
  *
- *	For tight control over page level allocator and protection flags
- *	use __vmalloc() instead.
+ *      For tight control over page level allocator and protection flags
+ *      use __vmalloc() instead.
  */
-void *vzalloc_zram(unsigned long size)
+void *vzalloc(unsigned long size)
 {
 	return __vmalloc_node_flags(size, -1,
-				GFP_KERNEL | __GFP_HIGHMEM | __GFP_ZERO);
+					GFP_KERNEL | __GFP_HIGHMEM | __GFP_ZERO);
 }
-EXPORT_SYMBOL(vzalloc_zram);
+EXPORT_SYMBOL(vzalloc);
+
 
 /**
  * vmalloc_user - allocate zeroed virtually contiguous memory for userspace
